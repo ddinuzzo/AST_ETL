cd75a74ddb7fb9d876f61274e5f2c5b5eda90a93
Danilo Di Nuzzo <danilo.dinuzzo@r-dev.it>
2020-11-26 02:45:14 +0100

    update slide + test

---
+++ esercizi/ragione_sociale/Esercizio1.sql
@@ -0,0 +1,69 @@
+-- DROP TABLE public.clienti;
+
+--sintassi potgresql
+--CREATE TABLE public.clienti (
+	--id_cliente serial NOT NULL DEFAULT nextval('clienti_id_cliente_seq'::regclass),
+	--ragione_sociale varchar(150) NOT NULL,
+	--sconto int4 NOT NULL
+--);
+
+insert into clienti (ragione_sociale, sconto) values ('Five Star Senior Living Inc.', 2);
+insert into clienti (ragione_sociale, sconto) values ('Sprott Focus Trust, Inc.', 47);
+insert into clienti (ragione_sociale, sconto) values ('L.S. Starrett Company (The)', 29);
+insert into clienti (ragione_sociale, sconto) values ('James Hardie Industries plc.', 34);
+insert into clienti (ragione_sociale, sconto) values ('Container Store (The)', 11);
+insert into clienti (ragione_sociale, sconto) values ('Copart, Inc.', 12);
+insert into clienti (ragione_sociale, sconto) values ('Mechel PAO', 44);
+insert into clienti (ragione_sociale, sconto) values ('Nuveen New Jersey Quality Municipal Income Fund', 45);
+insert into clienti (ragione_sociale, sconto) values ('Archrock, Inc.', 10.);
+insert into clienti (ragione_sociale, sconto) values ('Vanguard International Dividend Appreciation ETF', 22);
+insert into clienti (ragione_sociale, sconto) values ('Tortoise Energy Independence Fund, Inc.', 31);
+insert into clienti (ragione_sociale, sconto) values ('Royal Bank Scotland plc (The)', 2);
+insert into clienti (ragione_sociale, sconto) values ('Western Asset Emerging Markets Debt Fund Inc', 20);
+insert into clienti (ragione_sociale, sconto) values ('Aradigm Corporation', 10);
+insert into clienti (ragione_sociale, sconto) values ('Affimed N.V.', 2);
+insert into clienti (ragione_sociale, sconto) values ('L.B. Foster Company', 3);
+insert into clienti (ragione_sociale, sconto) values ('Heska Corporation', 20);
+insert into clienti (ragione_sociale, sconto) values ('iShares MSCI EAFE ESG Optimized ETF', 28);
+insert into clienti (ragione_sociale, sconto) values ('Athenex, Inc.', 1);
+insert into clienti (ragione_sociale, sconto) values ('Canadian Natural Resources Limited', 18);
+insert into clienti (ragione_sociale, sconto) values ('FactSet Research Systems Inc.', 31);
+insert into clienti (ragione_sociale, sconto) values ('Torchmark Corporation', 16);
+insert into clienti (ragione_sociale, sconto) values ('Meritor, Inc.', 6);
+insert into clienti (ragione_sociale, sconto) values ('Avianca Holdings S.A.', 26);
+insert into clienti (ragione_sociale, sconto) values ('Biostage, Inc.', 8);
+insert into clienti (ragione_sociale, sconto) values ('Allergan plc.', 24);
+insert into clienti (ragione_sociale, sconto) values ('Sientra, Inc.', 10);
+insert into clienti (ragione_sociale, sconto) values ('CAI International, Inc.', 44);
+insert into clienti (ragione_sociale, sconto) values ('Aegon NV', 21);
+insert into clienti (ragione_sociale, sconto) values ('Paychex, Inc.', 44);
+insert into clienti (ragione_sociale, sconto) values ('Turning Point Brands, Inc.', 40);
+insert into clienti (ragione_sociale, sconto) values ('Prudential Financial, Inc.', 32);
+insert into clienti (ragione_sociale, sconto) values ('Trimble Inc.', 7);
+insert into clienti (ragione_sociale, sconto) values ('National Retail Properties', 48);
+insert into clienti (ragione_sociale, sconto) values ('Gabelli Convertible and Income Securities Fund, Inc.', 30);
+insert into clienti (ragione_sociale, sconto) values ('Nuveen Virginia Quality Municipal Income Fund', 36);
+insert into clienti (ragione_sociale, sconto) values ('Cartesian, Inc.', 41);
+insert into clienti (ragione_sociale, sconto) values ('BlackRock Capital Investment Corporation', 11);
+insert into clienti (ragione_sociale, sconto) values ('CenterState Banks, Inc.', 36);
+insert into clienti (ragione_sociale, sconto) values ('CommScope Holding Company, Inc.', 43);
+insert into clienti (ragione_sociale, sconto) values ('FIRST REPUBLIC BANK', 49);
+insert into clienti (ragione_sociale, sconto) values ('Sealed Air Corporation', 6);
+insert into clienti (ragione_sociale, sconto) values ('Cosan Limited', 31);
+insert into clienti (ragione_sociale, sconto) values ('Texas Capital Bancshares, Inc.', 31);
+insert into clienti (ragione_sociale, sconto) values ('Superior Uniform Group, Inc.', 47);
+insert into clienti (ragione_sociale, sconto) values ('CNX Coal Resources LP', 47);
+insert into clienti (ragione_sociale, sconto) values ('Flex Ltd.', 11);
+insert into clienti (ragione_sociale, sconto) values ('Hawaiian Electric Industries, Inc.', 34);
+insert into clienti (ragione_sociale, sconto) values ('Automatic Data Processing, Inc.', 10);
+insert into clienti (ragione_sociale, sconto) values ('Royal Bank Scotland plc (The)', 15);
+insert into clienti (ragione_sociale, sconto) values ('Five Star Senior Living Inc.', 10);
+insert into clienti (ragione_sociale, sconto) values ('Sprott Focus Trust, Inc.', 16);
+insert into clienti (ragione_sociale, sconto) values ('L.S. Starrett Company (The)', 12);
+insert into clienti (ragione_sociale, sconto) values ('James Hardie Industries plc.', 14);
+insert into clienti (ragione_sociale, sconto) values ('Container Store (The)', 31);
+insert into clienti (ragione_sociale, sconto) values ('Copart, Inc.', 22);
+insert into clienti (ragione_sociale, sconto) values ('Mechel PAO', 12);
+insert into clienti (ragione_sociale, sconto) values ('Nuveen New Jersey Quality Municipal Income Fund', 12);
+insert into clienti (ragione_sociale, sconto) values ('Archrock, Inc.', 40);
+insert into clienti (ragione_sociale, sconto) values ('Copart, Inc.', 19);
